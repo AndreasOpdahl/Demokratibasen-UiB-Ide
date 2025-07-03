b06a1717321d544868900356db15f8748cf71f9d
Snorre Aaldstedt <snorrealdstedt@gmail.com>
2025-07-03 02:55:29 +0200

    Summarize list of kode2 given by models

--- models_classify/claude_test.py
+++ models_classify/claude_test.py
@@ -1,3 +1,143 @@
-test_list = ["internt notat", "administrativt vedtak", "tegninger", "m\u00f8teprotokoll", "m\u00f8teprotokoll", "m\u00f8teprotokoll", "forvaltningsrevisjon", "\u00f8konomiske rapporter", "internt notat", "h\u00f8ringsdokument", "internt notat", "kart", "klager", "h\u00f8ringsdokumenter", "internt notat", "m\u00f8tereferat", "h\u00f8ringsdokument", "s\u00f8knad", "internt notat", "m\u00f8teplan", "tegninger", "klage", "redegj\u00f8relse", "alternativanalyse", "reguleringsplaner", "sp\u00f8rsm\u00e5l fra/til politikere", "tekniske rapporter", "h\u00f8ringsdokumenter", "h\u00f8ringsdokumenter", "tegninger", "sp\u00f8rsm\u00e5l fra/til politikere", "administrativt vedtak", "s\u00f8knad", "intern rapport", "internt notat", "internt notat", "tegninger", "m\u00f8teprotokoll", "administrativt vedtak", "klage", "teknisk rapport", "h\u00f8ringsdokument", "klage", "tegninger", "m\u00f8teprotokoll", "reguleringsplaner", "h\u00f8ringsdokumenter", "klager", "administrativt vedtak", "Reguleringsplaner", "gjennomf\u00f8ringsvedtak", "saksprotokoll", "foto", "klage", "h\u00f8ringsdokumenter", "Planbeskrivelse", "byggesaker", "m\u00f8teplan", "tegninger", "Tilleggsnotat", "handlingsplan", "klage", "planbeskrivelse", "nabomerknad", "administrativt vedtak", "tertialrapport", "s\u00f8knad", "foto", "administrativt vedtak", "representantforslag", "kart", "m\u00f8teprotokoll", "vedtak", "h\u00f8ringsdokumenter", "saksfremlegg", "dispensasjoner", "h\u00f8ringsdokumenter", "kart", "\u00f8konomiske rapporter", "teknisk rapport", "h\u00f8ringsdokument", "kommunedelplan", "m\u00f8teplan", "\u00f8konomiske s\u00f8knader", "internt notat", "m\u00f8teplan", "m\u00f8teplan", "dispensasjonss\u00f8knad", "teknisk rapport", "h\u00f8ringsdokument", "planoppstart", "byggesaker", "klage", "foto", "intern_uttalelse", "representantforslag", "saksprotokoll", "h\u00f8ringsdokumenter", "h\u00f8ringsdokument", "foto", "interne notater", "internt notat", "internt notat", "h\u00f8ringsdokument", "klage", "rammetillatelse", "internt notat", "representantforslag", "forslag", "klager", "forskrift", "reguleringsplaner", "tegninger", "klager", "m\u00f8tereferat", "h\u00f8ringsdokumenter", "klage", "saksfremlegg", "forslag", "administrativt vedtak", "forslag", "foto", "m\u00f8teplan", "Reguleringsplaner", "s\u00f8knad", "interne notater", "budsjett-innspill", "erkl\u00e6ringer", "klage", "klage", "sp\u00f8rsm\u00e5l fra/til politikere", "klage", "Reguleringsplaner", "saksprotokoll", "h\u00f8ringsdokumenter", "oppstartssak", "klage", "byggesaker", "rammetillatelser", "\u00f8konomiske rapporter", "Reguleringsplaner", "administrativt vedtak", "innspill", "kart", "klage", "sp\u00f8rsm\u00e5l fra/til politikere", "h\u00f8ringsdokumenter", "saksprotokoll", "m\u00f8teprotokoll", "h\u00f8ringsdokumenter", "presentasjon", "m\u00f8teplan", "klage", "Reguleringsplaner", "Reguleringsplaner", "h\u00f8ringsdokumenter", "innspillsdokument", "innbyggerforslag", "representantforslag", "s\u00f8knad", "internt notat", "klage", "reguleringsplaner", "forslag", "kulturminnedokumentasjon", "s\u00f8knad", "internt notat", "teknisk rapport", "tekniske rapporter", "klage", "tekniske rapporter", "klage", "protokoll", "internt notat", "administrativt vedtak", "h\u00f8ringsdokument", "klager", "nabovarsel", "s\u00f8knad", "naturrapport", "rammetillatelse", "innspill", "forslag", "s\u00f8knad", "innstilling-vedtak", "avslag", "h\u00f8ringsdokument", "sp\u00f8rsm\u00e5l fra/til politikere", "representantforslag", "sluttmelding", "m\u00f8teplan", "kart", "h\u00f8ringsdokument", "kulturminnedokumentasjon", "internt notat", "klage", "klage", "samarbeidsavtale", "administrativt vedtak", "internt notat", "administrativt vedtak", "saksprotokoll", "administrativt vedtak", "strategi", "\u00f8konomiske rapporter", "representantforslag", "klage", "administrativt vedtak", "reguleringsplaner", "sp\u00f8rsm\u00e5l fra/til politikere", "m\u00f8teplan", "m\u00f8teprotokoll", "\u00f8konomisk rapport", "m\u00f8tereferat", "klager", "h\u00f8ringsdokumenter", "h\u00f8ringsdokumenter", "internt notat", "h\u00f8ringsdokument", "m\u00f8teplan", "m\u00f8teplan", "revisjonsrapport", "m\u00f8teplan", "administrativt vedtak", "klager", "tilsvar nabomerknader", "konkurransegrunnlag", "kart", "h\u00f8ringsdokumenter", "revisjonsrapport", "internt notat", "administrativt vedtak", "klage", "kart", "sp\u00f8rsm\u00e5l fra/til politikere", "internt notat", "sluttmelding", "m\u00f8teprotokoll", "\u00f8konomiske rapporter", "protokoll", "fagnotat", "kart", "statusmelding", "reguleringsplaner", "klager", "Reguleringsplaner", "reguleringsplaner", "m\u00f8teprotokoll", "Reguleringsplaner", "forslag", "m\u00f8teplan", "Formingsveileder", "fagnotat", "uttalelse", "s\u00f8knad", "tegninger", "administrativt vedtak", "h\u00f8ringsdokumenter", "h\u00f8ringsdokumenter", "internt notat", "tegninger", "s\u00f8knad", "m\u00f8teprotokoll", "interne notater", "m\u00f8teplan", "dispensasjonss\u00f8knad", "byggesaker", "rammetillatelse", "s\u00f8knad", "internt_notat", "reguleringsplaner", "foto", "rammetillatelse", "reguleringsplaner", "artikkel", "kart", "administrativt vedtak", "h\u00f8ringsdokument", "sp\u00f8rsm\u00e5l fra/til politikere", "tekniske rapporter", "m\u00f8teprotokoll", "m\u00f8teplan", "\u00f8konomiske rapporter", "klager", "kompetanseskjema", "administrativt vedtak", "administrativt vedtak", "h\u00f8ringsdokument", "klage", "stedsanalyse", "m\u00f8tereferat", "klage", "fagnotat", "internt notat", "budsjettforslag", "h\u00f8ringsdokumenter", "handlingsplan", "m\u00f8teprotokoll", "fagnotat", "uttalelse", "inng\u00e5ende brev", "forvaltningsrevisjonsrapport", "revisjonsrapport", "foto", "forslag", "tegninger", "h\u00f8ringsdokumenter", "foto", "Reguleringsplaner", "kart", "innkalling", "tegninger", "h\u00f8ringsdokumenter", "administrativt vedtak", "byggesaker", "m\u00f8tereferat", "h\u00f8ringsdokumenter", "kart", "m\u00f8teplan", "internt notat", "protokoll", "administrativt vedtak", "m\u00f8teprotokoll", "svar p\u00e5 representantforslag", "administrativt vedtak", "Reguleringsplaner", "internt notat", "m\u00f8teprotokoll", "tegninger", "h\u00f8ringsdokumenter", "sp\u00f8rsm\u00e5l fra/til politikere", "tegninger", "innbyggerforslag", "klager", "h\u00f8ringsdokumenter", "klage", "h\u00f8ringsdokument", "klage", "administrativt vedtak", "fagnotat", "\u00f8konomiske rapporter", "h\u00f8ringsdokumenter", "internt notat", "\u00f8konomiske rapporter", "sp\u00f8rsm\u00e5l fra/til politikere", "tegninger", "klager", "teknisk rapport", "rammetillatelse", "foto", "sp\u00f8rsm\u00e5l fra/til politikere", "brev", "foto", "m\u00f8teplan", "Reguleringsplaner", "sp\u00f8rsm\u00e5l fra/til politikere", "h\u00f8ringsdokument", "teknisk rapport", "tegninger", "administrativt vedtak", "sp\u00f8rsm\u00e5l fra/til politikere", "internt notat", "\u00f8konomisk rapport", "s\u00f8knad", "m\u00f8teprotokoll", "kart", "m\u00f8teplan", "s\u00f8knad", "m\u00f8teplan", "s\u00f8knad", "foredrag", "klage", "situasjonsplan", "klage", "tegninger", "s\u00f8knad", "h\u00f8ringsdokumenter", "administrativt vedtak", "Reguleringsplaner", "saksframlegg", "reguleringsplaner", "m\u00f8teprotokoll", "Reguleringsplaner", "administrativt vedtak", "klage", "klage", "protokoll", "h\u00f8ringsdokument", "m\u00f8teprotokoll", "rammetillatelser", "kart", "vedlikeholdsplan", "revisjonsrapporter", "kulturminnedokumentasjon", "s\u00f8knad", "m\u00f8teprotokoll", "sp\u00f8rsm\u00e5l fra/til politikere", "m\u00f8teplan", "sp\u00f8rsm\u00e5l fra/til politikere", "h\u00f8ringsdokument", "vedtak", "h\u00f8ringsdokumenter", "internt notat", "sp\u00f8rsm\u00e5l fra/til politikere", "handlingsplan", "administrativt vedtak", "s\u00f8knad", "klage", "tegninger", "tegninger", "teknisk rapport", "reguleringsplaner", "tegninger", "revisjonsrapporter", "rammetillatelse", "handlingsplan", "erkl\u00e6ringer", "h\u00f8ringsdokumenter", "interne notater", "administrativt vedtak", "h\u00f8ringsdokument", "reguleringsplaner", "reguleringsplaner", "administrativt vedtak", "inng\u00e5ende brev", "saksprotokoll", "internt notat", "s\u00f8knad", "reguleringsplaner", "administrativt vedtak", "h\u00f8ringsdokument", "\u00f8konomiske rapporter", "Reguleringsplaner", "h\u00f8ringsdokument", "Reguleringsplaner", "tegninger", "klager", "administrativt vedtak", "internt notat", "h\u00f8ringsdokument", "kommunedelplan", "m\u00f8teplan", "representantforslag", "klage", "m\u00f8teprotokoll", "h\u00f8ringsdokumenter", "h\u00f8ringsdokumenter", "\u00f8konomiske rapporter", "foto", "h\u00f8ringsdokumenter", "klage", "m\u00f8tereferat", "klager", "klage", "dispensasjonss\u00f8knad", "forslag", "h\u00f8ringsdokumenter", "administrativt vedtak", "h\u00f8ringsdokumenter", "skriftlige orienteringer", "Reguleringsplaner", "h\u00f8ringsdokumenter", "h\u00f8ringsdokumenter", "klage", "m\u00f8teplan", "reguleringsplaner", "administrativt vedtak", "klage", "teknisk rapport", "h\u00f8ringsdokumenter", "m\u00f8teprotokoll", "h\u00f8ringsdokument", "administrativt vedtak", "reguleringsplaner", "revisjonsrapport", "m\u00f8teplan", "sp\u00f8rsm\u00e5l fra/til politikere", "\u00f8konomiske rapporter", "uttale", "m\u00f8teplan", "foto", "tegninger", "\u00f8konomiske rapporter", "\u00f8konomiske rapporter", "m\u00f8teprotokoll", "m\u00f8teprotokoll", "erkl\u00e6ring", "m\u00f8teplan", "interne notater", "klage", "m\u00f8teprotokoll", "forskrift", "fagnotat", "h\u00f8ringsdokument", "h\u00f8ringsdokument", "m\u00f8teplan", "reguleringsplaner"]
+import os
+import json
+from google import genai
+import openai
+import anthropic
+from google.genai import types
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from structured_documenttype_class import extract_json
 
-print(len(set(test_list)))
+load_dotenv()
+class DokumenttypeKode2(BaseModel):
+    kode2: str
+    begrunnelse_kode2: str
+
+class Kode2Liste(BaseModel):
+    liste: list[DokumenttypeKode2]
+
+
+PROMPT_SYSTEM = """Du er en ekspert på å oppsummere kategorier. 
+Du får inn en liste med par `[("<en kategori>", "<kort forklaring>")]`. Den korte forklaringen er en forklaring på hvorfor denne koden ble brukt tidligere.
+"""
+MODEL_PROMPT = """
+Her er dataen som er en liste med par med kategori og en forklaring på hvorfor den ble brukt for et dokument:
+{pairs}
+
+**Din oppgave**  
+- Lag cirka 40 `kode2`-elementer som sammen gir en god dekningsgrad og dekker bredden i dataene.  
+- Lag en forklaring til hva denne koden betyr med `forklaring` til hvert valgt `kode`.  
+- Output skal være **ren JSON**, en liste av objekter i samme format, slik:
+
+```json
+[
+  {{ "kode2": "…", "forklaring": "…" }},
+  {{ "kode2": "…", "forklaring": "…" }},
+  …
+]
+"""
+def create_unique_code(dict_list) -> set:
+    kode2_list = [e["kode"] for e in dict_list]
+    unique = set(kode2_list)
+    return unique
+
+#Function that finds the first "begrunnelse" for a "kode"
+def find_begrunnelse(model_list, code):
+    for d in model_list:
+        if d["kode"] == code:
+            return d["begrunnelse"]
+
+def create_kode_begrunnelse_pair(unique_list, model_list):
+    unique_begrunnelse = [(unique_kode, find_begrunnelse(model_list, unique_kode)) for unique_kode in unique_list]
+    return unique_begrunnelse
+
+gpt_client = openai.OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+google_client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+claude_client = anthropic.Anthropic(
+    api_key=os.getenv("ANTHROPIC_API_KEY"),
+)
+
+def get_kode2_from_model(model, kode_begrunnelse_pair_list):
+    prompt = MODEL_PROMPT.format(pairs=str(kode_begrunnelse_pair_list) )
+    schema = Kode2Liste
+    if model == "openai":
+        resp = gpt_client.responses.parse(
+            model="gpt-4o-mini",
+            input=[
+                {"role": "system", "content": PROMPT_SYSTEM},
+                {"role": "user",   "content": prompt},
+            ],
+            text_format=schema,
+        )
+        json_response = resp.output_text
+    elif model == "claude":
+        # Claude 3.5 Haiku: prompt the system then the user, ask for JSON only
+        claude_resp = claude_client.messages.create(
+            model="claude-3-5-haiku-20241022",
+            system=PROMPT_SYSTEM,
+            max_tokens=8192,
+            messages=[
+                {"role": "user", "content": prompt},
+            ],
+            temperature=0.1,
+        )
+        full_response = claude_resp.content[0].text
+        #json_response = extract_json(full_response)
+        json_response = full_response
+
+
+    elif model == "gemini":
+        gem_resp = google_client.models.generate_content(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction=PROMPT_SYSTEM,
+                temperature=0.0,
+                response_mime_type="application/json",
+                response_schema=schema, 
+            ),
+            contents=prompt
+        )
+        json_response = gem_resp.text
+    return json_response
+
+def save_json(filename, json_file):
+# write all three into one JSON
+    with open(f"./models_classify/{filename}.json", "w") as outfile:
+        json.dump(json_file, outfile, indent=2)
+
+
+if __name__ == "__main__":
+    with open('./models_classify/kode2.json', 'r') as file:
+        kode2_dict = json.load(file)
+    
+    kode2_claude = kode2_dict["claude"]
+    kode2_openai = kode2_dict["openai"]
+    kode2_gemini = kode2_dict["gemini"]
+    
+    claude_unique = create_unique_code(kode2_claude)
+    openai_unique = create_unique_code(kode2_openai)
+    gemini_unique = create_unique_code(kode2_gemini)
+
+    print(f"Claude unique codes #{len(claude_unique)}")
+    print(f"openai unique codes #{len(openai_unique)}")
+    print(f"gemini unique codes #{len(gemini_unique)}")
+
+    claude_pair = create_kode_begrunnelse_pair(claude_unique, kode2_claude) 
+    openai_pair = create_kode_begrunnelse_pair(openai_unique, kode2_openai) 
+    gemini_pair = create_kode_begrunnelse_pair(gemini_unique, kode2_gemini) 
+    print(claude_pair[0])   
+    #print(str(claude_pair))
+    #openai_kode2 = get_kode2_from_model("openai", openai_pair)
+    claude_kode2 = get_kode2_from_model("claude", claude_pair)
+    #gemini_kode2 = get_kode2_from_model("gemini", gemini_pair)
+    # write all three into one JSON
+    #save_json("openai_kode2", openai_kode2)
+    #save_json("claude", claude_kode2)
+    #save_json("gemini_kode2", gemini_kode2)
+    #Claude doesnt support structured json output, so it's saved as .txt file instead
+    with open('./models_classify/claude_kode2.txt', 'w') as file:
+        file.write(claude_kode2)
