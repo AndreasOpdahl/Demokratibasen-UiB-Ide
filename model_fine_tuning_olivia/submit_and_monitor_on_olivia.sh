98d9c0b398142bfde4b6dc8b27ae8b85c0f807be
Andreas L Opdahl <Andreas.Opdahl@uib.no>
2025-10-22 11:32:52 +0200

    Run Gemma-2b trial on olivia

--- model_fine_tuning_olivia/submit_and_monitor_on_olivia.sh
+++ model_fine_tuning_olivia/submit_and_monitor_on_olivia.sh
@@ -9,3 +9,6 @@ sbatch run_finetune.sbatch
 squeue -u $USER
 # tail logs as it runs/finishes
 tail -f logs/gpu-smoke-*.out
+
+
+salloc -p accel --gres=gpu:1 --cpus-per-task=4 --mem=16G -A nn12075k --time=01:00:00
